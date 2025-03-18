8c7ccf76f6abd0eb5e2ae2ceab197afbf117796a
Farhan-Ganayim <f.ganayim@gmail.com>
2025-03-18 19:42:16 +0200

    Add bug backend service and basic server

--- server.js
+++ server.js
@@ -2,6 +2,26 @@ import express from 'express'
 
 const app = express()
 
+app.get('/api/bug', (req, res) => {
+    res.send('List of bugs')
+})
+
+app.get('/api/bug/save', (req, res) => {
+    res.send('Saving a bug or update')
+})
+
+app.get('/api/bug/:bugId', (req, res) => {
+    const { bugId } = req.params
+    console.log(req)
+    res.send(`Return bug with id: ${bugId}`)
+
+})
+
+app.get('/api/bug/:bugId/remove', (req, res) => {
+    const {bugId} = req.params
+    res.send(`Remove bug with id: ${bugId}`)
+})
+
 app.get('/', (req, res) => res.send('Hello there'))
 
 app.listen(3030, () => console.log('Server ready at port 3030'))
