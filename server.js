02f4185459969e00ae3520fef0cacdd85651a1b4
Farhan-Ganayim <f.ganayim@gmail.com>
2025-03-19 14:31:32 +0200

    Complete server.js to use service

--- server.js
+++ server.js
@@ -1,25 +1,52 @@
 import express from 'express'
+import { bugService } from './services/bug.service.js'
+import { loggerService } from './services/logger.service.js'
 
 const app = express()
 
 app.get('/api/bug', (req, res) => {
-    res.send('List of bugs')
+    bugService.query()
+        .then(bugs => res.send(JSON.stringify(bugs, null, 4)))
+        .catch(err => {
+            loggerService.error('Cannot get bugs', err)
+            res.status(500).send('Cannot load bugs')
+        })
 })
 
 app.get('/api/bug/save', (req, res) => {
-    res.send('Saving a bug or update')
+    const bugToSave = {
+        _id: req.query._id,
+        title: req.query.title,
+        description: req.query.severity,
+        severity: +req.query.severity,
+        createdAt: +req.query.createdAt
+    }
+    bugService.save(bugToSave)
+        .then(bug => res.send(bug))
+        .catch(err => {
+            loggerService.error('Cannot save bug', err)
+            res.status(500).send('Cannot save bug')
+        })
 })
 
 app.get('/api/bug/:bugId', (req, res) => {
     const { bugId } = req.params
-    console.log(req)
-    res.send(`Return bug with id: ${bugId}`)
-
+    bugService.getById(bugId)
+        .then(bug => res.send(bug))
+        .catch(err => {
+            loggerService.error('Cannot get bug', err)
+            res.status(500).send('Cannot get bug')
+        })
 })
 
 app.get('/api/bug/:bugId/remove', (req, res) => {
-    const {bugId} = req.params
-    res.send(`Remove bug with id: ${bugId}`)
+    const { bugId } = req.params
+    bugService.emove(bugId)
+        .then(() => res.send('Bug removed'))
+        .catch(err => {
+            loggerService.error('Cannot remove bug', err)
+            res.status(500).send('Cannot remove bug')
+        })
 })
 
 app.get('/', (req, res) => res.send('Hello there'))
